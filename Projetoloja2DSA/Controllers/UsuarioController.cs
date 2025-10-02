d3c4aa1ca48f8e0fc6d5c3279e8537783455b8ee
Paula <paulahildebrando2@gmail.com>
2025-10-02 17:03:03 -0300

    6-criando a model usuario

--- Projetoloja2DSA/Controllers/UsuarioController.cs
+++ Projetoloja2DSA/Controllers/UsuarioController.cs
@@ -9,25 +9,45 @@ namespace Projetoloja2DSA.Controllers
     //CLASSE USUARIOCONTROLLER QUE ESTA HERDADNDO DA CLASSE PAI CONTROLLER
     public class UsuarioController : Controller
     {
-        //declara a variavel privada e somente leitura do tipo usuariorepositorio
+        //declara a variavel privada e somente leitura do tipo UsuarioRepositorio
         //instancia o _usuarioController para ser atribuido no construtor com os dados
         private readonly UsuarioRepositorio _usuarioRepositorio;
 
-    //define o construtor da classe usuariocontroller
-    //recebe a instancia de usuariorepositorio com parametros
-}
-    public UsuarioController(UsuarioRepositorio usuarioRepositorio)
-    {
-        //o construtor é chamado quando uma nova instancia é criada
-        _usuarioRepositorio = usuarioRepositorio;
-    }
+        //define o construtor da classe UsuarioController
+        //recebe a instancia de usuariorepositorio com parametros
 
-
-    {
-        //IRTEFACE QUE REPRESENTA O RESULTADO PA PÁGINA
-        public IActionResult Index()
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
         {
+            //o construtor é chamado quando uma nova instancia é criada
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+        
+        [HttpGet]
+        //INTERFACE É UMA REPRESENTAÇAO DO RESULTADO (TELA)
+        public IActionResult Login()
+        {
+            //RETORNA A PAGINA INDEX
             return View();
         }
+
+        [HttpPost]
+                  //metodo login que recebe 2 parametros email e senha
+        public IActionResult Login(string email, string senha)
+        {
+            // variavel usuario
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+
+            if (usuario != null && usuario.senha != senha)
+            {
+                return RedirectToAction("Index", "Cliente");
+            }
+            ModelState.AddModelError("", "Email / senha Inválidos");
+
+
+            //RETORNA A PAGINA INDEX
+            return View();
+        }
+
+        
     }
 }
--- Projetoloja2DSA/Models/Usuario.cs
+++ Projetoloja2DSA/Models/Usuario.cs
@@ -2,5 +2,10 @@
 {
     public class Usuario
     {
+        public int Id{get;set;} //{get;set;} acessores realizando o encapsulmento dos dados
+        public string ?Nome{get;set;}
+        public string ?Email{get;set;}
+        public string ?Senha{get;set;}
+
     }
 }
